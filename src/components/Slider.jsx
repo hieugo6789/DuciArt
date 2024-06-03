dff031b6c46a472b57a80f84212ddca5964b5b89
hieugo6789 <hieugo240103@gmail.com>
2024-06-03 10:16:18 +0700

    update picture

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,13 +1,15 @@
 import { useState, useEffect } from "react";
-import slider1 from "../assets/images/slider-1.jpg";
-import slider2 from "../assets/images/slider-2.jpg";
-import slider3 from "../assets/images/slider-3.jpg";
+import slider1 from "../assets/images/IMG_2325.jpg";
+import slider2 from "../assets/images/IMG_2320.jpg";
+import slider3 from "../assets/images/IMG_2314.jpg";
+import slider4 from "../assets/images/IMG_2347.jpg";
 import classes from "./Slider.module.css";
 
 const images = [
   { image: slider1, alt: "A delicious, juicy burger" },
   { image: slider2, alt: "A delicious, spicy curry" },
   { image: slider3, alt: "Steamed dumplings" },
+  { image: slider4, alt: "Steamed dumplings" },
 ];
 
 const Slider = () => {
